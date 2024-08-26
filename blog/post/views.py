afbd6b0de6994cf05a2f7739a5c273b1ec0ef4a2
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-25 22:23:44 -0500

    Creacion de pagina de inicio de los blogs

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import  Post
 
 # Create your views here.
+def post(request):
+    post = Post.objects.all()
+    return render(request, 'post.html', {'post': post})
+
+def inicio(request):
+    pass
+    return render(request, 'inicio.html')
