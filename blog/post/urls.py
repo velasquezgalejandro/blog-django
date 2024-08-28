04e3468d5c9dda614c7c6967a74a62407bacf07f
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-28 18:26:11 -0500

    creación de template de listar post y implementación en views y urls

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post, agregar_post
+from .views import post, agregar_post, listar_post
 
 urlpatterns = [
     path('post/', post, name='post'),
     path('agregar_post/', agregar_post, name='agregar_post'),
+    path('listar_post/', listar_post, name='listar_post'),
 ]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 from .models import  Post
 from .forms import  PostModelForm
 
@@ -10,6 +13,21 @@ def post(request):
 def inicio(request):
     pass
     return render(request, 'inicio.html')
+
+def listar_post(request):
+    posts_list = Post.objects.all()
+    query = request.GET.get('q')
+    paginator = Paginator(posts_list, 10)
+
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    # if precio_min and precio_max:
+    #     posts_list = posts_list.filter(precio__gte = precio_min, precio__lte= precio_max)
+
+    if query:
+        posts_list = posts_list.filter(Q(titulo__icontains = query)| Q(contenido__icontains = query) | Q(categoria__icontains = query))
+    return render(request, 'listar_post.html', {'posts': posts})
 
 def agregar_post(request):
     if request.method == 'POST':
