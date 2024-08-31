f31199f4c67a878db2f4e9306a312fbc32eb0b08
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-30 19:04:48 -0500

    implementacion de urls para editar post y vista de detalle

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import post, agregar_post, listar_post
+from .views import post, agregar_post, listar_post, editar_post, detalle_post
 
 urlpatterns = [
     path('post/', post, name='post'),
     path('agregar_post/', agregar_post, name='agregar_post'),
+    path('editar_post/<int:pk>/', editar_post, name='editar_post'),
     path('listar_post/', listar_post, name='listar_post'),
+    path('post/<int:pk>/', detalle_post, name='detalle_post'),
 ]
